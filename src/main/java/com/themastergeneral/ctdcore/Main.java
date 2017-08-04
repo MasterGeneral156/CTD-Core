74bd0b1fde92baa9b6a0dadd71b384a874c5cd3c
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-08-04 10:28:50 -0400

    Updated to 1.12.1

--- src/main/java/com/themastergeneral/ctdcore/Main.java
+++ src/main/java/com/themastergeneral/ctdcore/Main.java
@@ -25,7 +25,7 @@ public class Main
     public static final String MODNAME = "CTD Core";
     public static final String VERSION = "1.1.6";
     public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Core.json";
-    public static final String acceptedMinecraftVersions = "1.12";
+    public static final String acceptedMinecraftVersions = "1.12.1";
     
     @SidedProxy(clientSide="com.themastergeneral.ctdcore.proxy.ClientProxy", serverSide="com.themastergeneral.ctdcore.proxy.ServerProxy")
     public static CommonProxy proxy;
