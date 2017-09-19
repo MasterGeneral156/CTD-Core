7365b0ffe602e6e24fd8b73f86eef896f806101a
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-19 12:54:34 -0400

    1.12.2 update

--- src/main/java/com/themastergeneral/ctdcore/Main.java
+++ src/main/java/com/themastergeneral/ctdcore/Main.java
@@ -16,8 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Main.MODID, 
 		name = Main.MODNAME, 
-		version = Main.VERSION, 
-		acceptedMinecraftVersions = Main.acceptedMinecraftVersions, 
+		version = Main.VERSION,  
 		updateJSON = Main.updateJSON)
 public class Main 
 {
@@ -25,7 +24,6 @@ public class Main
     public static final String MODNAME = "CTD Core";
     public static final String VERSION = "1.1.6";
     public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Core.json";
-    public static final String acceptedMinecraftVersions = "1.12.1";
     
     @SidedProxy(clientSide="com.themastergeneral.ctdcore.proxy.ClientProxy", serverSide="com.themastergeneral.ctdcore.proxy.ServerProxy")
     public static CommonProxy proxy;
