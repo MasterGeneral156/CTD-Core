17018fc1a46a5e7ee628df5261c66a00361abce1
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-20 08:09:41 -0400

    Allows loading on all 1.12.x versions

--- src/main/java/com/themastergeneral/ctdcore/Main.java
+++ src/main/java/com/themastergeneral/ctdcore/Main.java
@@ -14,44 +14,39 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Main.MODID, 
-		name = Main.MODNAME, 
-		version = Main.VERSION,  
-		updateJSON = Main.updateJSON)
-public class Main 
-{
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION, acceptedMinecraftVersions = Main.acceptedMinecraftVersions, updateJSON = Main.updateJSON)
+public class Main {
 	public static final String MODID = "ctdcore";
-    public static final String MODNAME = "CTD Core";
-    public static final String VERSION = "1.1.6";
-    public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Core.json";
-    
-    @SidedProxy(clientSide="com.themastergeneral.ctdcore.proxy.ClientProxy", serverSide="com.themastergeneral.ctdcore.proxy.ServerProxy")
-    public static CommonProxy proxy;
-    
-    @Instance
-    public static Main instance = new Main();
-    public static Logger logger;
-    static 
-    {
-    	FluidRegistry.enableUniversalBucket();
-    }
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent e) 
-    {
-    	logger = e.getModLog();
-    	logger.info("Starting CTD Core.");
-    	proxy.preInit(e);
-    }
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent e) 
-    {
-    	proxy.init(e);
-    }
-        
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent e) 
-    {
-    	proxy.postInit(e);
-    	logger.info("Loaded CTD Core.");
-    }
+	public static final String MODNAME = "CTD Core";
+	public static final String VERSION = "1.1.7";
+	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Core.json";
+	public static final String acceptedMinecraftVersions = "1.12.2";
+
+	@SidedProxy(clientSide = "com.themastergeneral.ctdcore.proxy.ClientProxy", serverSide = "com.themastergeneral.ctdcore.proxy.ServerProxy")
+	public static CommonProxy proxy;
+
+	@Instance
+	public static Main instance = new Main();
+	public static Logger logger;
+	static {
+		FluidRegistry.enableUniversalBucket();
+	}
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		logger = e.getModLog();
+		logger.info("Starting CTD Core.");
+		proxy.preInit(e);
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent e) {
+		proxy.init(e);
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent e) {
+		proxy.postInit(e);
+		logger.info("Loaded CTD Core.");
+	}
 }
