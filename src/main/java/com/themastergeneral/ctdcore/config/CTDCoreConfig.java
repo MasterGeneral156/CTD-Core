7fc8d3ac03328ac85c3022ea81e34b0e1c3d961f
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-26 10:08:05 -0400

    Added config

---
+++ src/main/java/com/themastergeneral/ctdcore/config/CTDCoreConfig.java
@@ -0,0 +1,36 @@
+package com.themastergeneral.ctdcore.config;
+
+import org.apache.logging.log4j.Level;
+
+import net.minecraftforge.common.config.Configuration;
+
+import com.themastergeneral.ctdcore.Main;
+import com.themastergeneral.ctdcore.proxy.CommonProxy;
+
+public class CTDCoreConfig {
+	private static final String CATEGORY_GENERAL = "General";
+	public static boolean disableTMGDrop = false;
+
+	public static void readConfig() {
+		Configuration cfg = CommonProxy.config;
+		try {
+			cfg.load();
+			initGeneralConfig(cfg);
+		} catch (Exception e1) {
+			Main.logger.log(Level.ERROR, "Problem loading config file!", e1);
+		} finally {
+			if (cfg.hasChanged()) {
+				cfg.save();
+			}
+		}
+	}
+
+	private static void initGeneralConfig(Configuration cfg) {
+		disableTMGDrop = cfg
+				.getBoolean(
+						"Disable TheMasterGeneral's Drop",
+						CATEGORY_GENERAL,
+						disableTMGDrop,
+						"Everytime you kill TheMasterGeneral, he will drop his head when this is enabled.");
+	}
+}
