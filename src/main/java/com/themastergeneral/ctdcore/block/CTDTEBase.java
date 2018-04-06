2aea3f8c85ba6d0bda98dd55d25659a85b7fd51b
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-04-06 15:05:08 -0400

    Add TE Base to CTD Core for use in other mods

---
+++ src/main/java/com/themastergeneral/ctdcore/block/CTDTEBase.java
@@ -0,0 +1,33 @@
+package com.themastergeneral.ctdcore.block;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public abstract class CTDTEBase<TE extends TileEntity> extends CTDBlock {
+
+	public CTDTEBase(Material material, String name, String ModID) {
+		super(material, name, ModID);
+	}
+	
+	public abstract Class<TE> getTileEntityClass();
+	
+	public TE getTileEntity(IBlockAccess world, BlockPos pos) {
+		return (TE)world.getTileEntity(pos);
+	}
+	
+	@Override
+	public boolean hasTileEntity(IBlockState state) {
+		return true;
+	}
+	
+	@Nullable
+	@Override
+	public abstract TE createTileEntity(World world, IBlockState state);
+
+}
