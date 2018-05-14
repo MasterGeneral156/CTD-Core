5774da33ea386a0c3994214f624d790f44ded21b
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-05-13 20:22:54 -0400

    Internal CTD Mythos IMC, for use in other mods

---
+++ src/main/java/com/themastergeneral/ctdcore/imc/CTDMythos.java
@@ -0,0 +1,95 @@
+package com.themastergeneral.ctdcore.imc;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+public class CTDMythos {
+
+	public static final void addFlightItem(ItemStack input, int flighttime,
+			int flightmultiplier, int resistance) {
+		if (input == null || flighttime < 0 || flightmultiplier < 0
+				|| resistance < 0) {
+			return;
+		}
+		NBTTagCompound toSend = new NBTTagCompound();
+		toSend.setTag("input", new NBTTagCompound());
+		toSend.setTag("flighttime", new NBTTagCompound());
+		toSend.setTag("resistance", new NBTTagCompound());
+		toSend.setTag("flightmod", new NBTTagCompound());
+		input.writeToNBT(toSend.getCompoundTag("input"));
+		toSend.setInteger("flighttime", flighttime);
+		toSend.setInteger("resistance", resistance);
+		toSend.setInteger("flightmod", flightmultiplier);
+		FMLInterModComms.sendMessage("ctdmythos", "add_flight_wand_item",
+				toSend);
+	}
+
+	public static final void removeFlightItem(ItemStack input) {
+		if (input == null) {
+			return;
+		}
+		NBTTagCompound toSend = new NBTTagCompound();
+		toSend.setTag("input", new NBTTagCompound());
+		input.writeToNBT(toSend.getCompoundTag("input"));
+		FMLInterModComms.sendMessage("ctdmythos", "remove_flight_wand_item",
+				toSend);
+	}
+
+	public static final void addInventoryCraft(ItemStack input,
+			ItemStack offhand, ItemStack output) {
+		if (input == null || output == null || offhand == null) {
+			return;
+		}
+		NBTTagCompound toSend = new NBTTagCompound();
+		toSend.setTag("input", new NBTTagCompound());
+		toSend.setTag("offhand", new NBTTagCompound());
+		toSend.setTag("output", new NBTTagCompound());
+		input.writeToNBT(toSend.getCompoundTag("input"));
+		offhand.writeToNBT(toSend.getCompoundTag("offhand"));
+		output.writeToNBT(toSend.getCompoundTag("output"));
+		FMLInterModComms.sendMessage("ctdmythos", "add_main_offhand_craft",
+				toSend);
+	}
+
+	public static final void removeInventoryCraft(ItemStack input,
+			ItemStack offhand, ItemStack output) {
+		if (input == null || output == null || offhand == null) {
+			return;
+		}
+		NBTTagCompound toSend = new NBTTagCompound();
+		toSend.setTag("input", new NBTTagCompound());
+		toSend.setTag("offhand", new NBTTagCompound());
+		toSend.setTag("output", new NBTTagCompound());
+		input.writeToNBT(toSend.getCompoundTag("input"));
+		offhand.writeToNBT(toSend.getCompoundTag("offhand"));
+		output.writeToNBT(toSend.getCompoundTag("output"));
+		FMLInterModComms.sendMessage("ctdmythos", "remove_main_offhand_craft",
+				toSend);
+	}
+
+	public static final void addPedestalCraft(ItemStack input, ItemStack output) {
+		if (input == null || output == null) {
+			return;
+		}
+		NBTTagCompound toSend = new NBTTagCompound();
+		toSend.setTag("input", new NBTTagCompound());
+		toSend.setTag("output", new NBTTagCompound());
+		input.writeToNBT(toSend.getCompoundTag("input"));
+		output.writeToNBT(toSend.getCompoundTag("output"));
+		FMLInterModComms.sendMessage("ctdmythos", "add_mb_craft", toSend);
+	}
+
+	public static final void removePedestalCraft(ItemStack input,
+			ItemStack output) {
+		if (input == null || output == null) {
+			return;
+		}
+		NBTTagCompound toSend = new NBTTagCompound();
+		toSend.setTag("input", new NBTTagCompound());
+		toSend.setTag("output", new NBTTagCompound());
+		input.writeToNBT(toSend.getCompoundTag("input"));
+		output.writeToNBT(toSend.getCompoundTag("output"));
+		FMLInterModComms.sendMessage("ctdmythos", "remove_mb_craft", toSend);
+	}
+}
