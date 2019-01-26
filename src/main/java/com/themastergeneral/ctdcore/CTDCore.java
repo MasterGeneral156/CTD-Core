27019f49810c47dda8d1d977b867a3d414a95f9e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-01-26 16:09:06 -0500

    Start to 1.13 port. Here goes nothing :D

--- src/main/java/com/themastergeneral/ctdcore/CTDCore.java
+++ src/main/java/com/themastergeneral/ctdcore/CTDCore.java
@@ -21,7 +21,7 @@ public class CTDCore {
 	public static final String MODNAME = "CTD Core";
 	public static final String VERSION = "1.4.1";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Core.json";
-	public static final String acceptedMinecraftVersions = "1.12.2";
+	public static final String acceptedMinecraftVersions = "1.13";
 	public static final String certificateFingerprint = "b50fe22651b9d97d9d6631514a6a57cd6174b0dc";
 
 	@SidedProxy(clientSide = "com.themastergeneral.ctdcore.proxy.ClientProxy", serverSide = "com.themastergeneral.ctdcore.proxy.ServerProxy")
