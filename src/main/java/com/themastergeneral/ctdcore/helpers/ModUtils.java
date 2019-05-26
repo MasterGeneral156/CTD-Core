0d962636c79f6d8207c42081c65ee4bf96872674
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-05-26 13:44:27 -0400

    Some new things.

--- src/main/java/com/themastergeneral/ctdcore/helpers/ModUtils.java
+++ src/main/java/com/themastergeneral/ctdcore/helpers/ModUtils.java
@@ -2,6 +2,10 @@ package com.themastergeneral.ctdcore.helpers;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.fml.common.IFuelHandler;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModUtils {
@@ -11,5 +15,15 @@ public class ModUtils {
 
 	public static Block getOtherModBlock(String fqrn) {
 		return Block.getBlockFromName(fqrn);
+	}
+	
+	public static void removeSmeltRecipe(ItemStack smelt)
+	{
+		FurnaceRecipes.instance().getSmeltingList().remove(smelt);
+	}
+	
+	public static void addSmeltRecipe(ItemStack input, ItemStack output, float xp)
+	{
+		GameRegistry.addSmelting(input, output, xp);
 	}
 }
