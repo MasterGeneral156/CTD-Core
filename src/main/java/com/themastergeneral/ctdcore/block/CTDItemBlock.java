e955b1259bda07cbc2b58d316518c1c60387acd9
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2019-06-08 12:33:12 -0400

    eh?

---
+++ src/main/java/com/themastergeneral/ctdcore/block/CTDItemBlock.java
@@ -0,0 +1,16 @@
+package com.themastergeneral.ctdcore.block;
+
+import com.themastergeneral.ctdcore.client.BlockRenderRegister;
+import com.themastergeneral.ctdcore.client.ItemModelProvider;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class CTDItemBlock extends ItemBlock {
+
+	public CTDItemBlock(Block block) {
+		super(block);
+	}
+
+}
