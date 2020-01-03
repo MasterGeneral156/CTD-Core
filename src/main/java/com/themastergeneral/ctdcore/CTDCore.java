2b49baa1801ea88b57756af686d16ed648445bfc
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-01-02 22:31:32 -0500

    1.15.x port

--- src/main/java/com/themastergeneral/ctdcore/CTDCore.java
+++ src/main/java/com/themastergeneral/ctdcore/CTDCore.java
@@ -1,12 +1,9 @@
 package com.themastergeneral.ctdcore;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.fml.config.ModConfig;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/com/themastergeneral/ctdcore/helpers/WorldHelper.java
+++ src/main/java/com/themastergeneral/ctdcore/helpers/WorldHelper.java
@@ -13,9 +13,9 @@ public class WorldHelper {
 
 	// Function used to find the block under an entity.
 	public static Block findBlockUnderEntity(Entity e) {
-		int blockX = MathHelper.floor(e.posX);
-		int blockY = MathHelper.floor(e.posY-0.2D - e.getYOffset());
-		int blockZ = MathHelper.floor(e.posZ);
+		int blockX = MathHelper.floor(e.serverPosX);
+		int blockY = MathHelper.floor(e.serverPosY-0.2D - e.getYOffset());
+		int blockZ = MathHelper.floor(e.serverPosZ);
 		BlockPos blockpos = new BlockPos(blockX, blockY, blockZ);
 		return e.world.getBlockState(blockpos).getBlock();
 	}
