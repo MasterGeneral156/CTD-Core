7b9a18eee5dcebe78227fc6926c16e585c6e4a45
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-07-10 10:36:14 -0400

    cont of prev commit

--- src/main/java/com/themastergeneral/ctdcore/CTDCore.java
+++ src/main/java/com/themastergeneral/ctdcore/CTDCore.java
@@ -20,16 +20,12 @@ public class CTDCore {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-        // Register ourselves for server, registry and other game events we are interested in
-        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CTDCoreConfig.COMMON_CONFIG);
-        //CTDCoreConfig.loadConfig(CTDCoreConfig.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("ctd/ctd-core.toml"));
         MinecraftForge.EVENT_BUS.register(this);
     }
 	
 	private void setup(final FMLCommonSetupEvent event)
     {
 		LOGGER.info("CTD Core is launching.");
-		LOGGER.warn("Please note that CTD Core is NOT feature complete.");
     }
 
 }
