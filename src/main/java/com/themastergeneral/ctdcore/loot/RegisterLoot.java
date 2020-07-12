5210f681bf292d94bad0d5ae0a0078ec078186f2
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-07-12 00:35:03 -0400

    Remove unneeded code.

--- src/main/java/com/themastergeneral/ctdcore/loot/RegisterLoot.java
+++
@@ -1,7 +0,0 @@
-package com.themastergeneral.ctdcore.loot;
-
-public class RegisterLoot {
-	public static void register(String modid, String lootTableName) {
-		//LootTableList.register(new ResourceLocation(modid, lootTableName));
-	}
-}
