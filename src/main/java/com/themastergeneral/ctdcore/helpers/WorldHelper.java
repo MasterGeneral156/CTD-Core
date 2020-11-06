9bb1dca496076c0a35e7d7fbbc5a3dd9b913a175
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-11-06 17:56:31 -0500

    Small fix needed here.

--- src/main/java/com/themastergeneral/ctdcore/helpers/WorldHelper.java
+++ src/main/java/com/themastergeneral/ctdcore/helpers/WorldHelper.java
@@ -14,9 +14,9 @@ public class WorldHelper
 	// Function used to find the block under an entity.
 	public static Block findBlockUnderEntity(Entity e) 
 	{
-		int blockX = MathHelper.floor(e.serverPosX);
-		int blockY = MathHelper.floor(e.serverPosY-0.2D - e.getYOffset());
-		int blockZ = MathHelper.floor(e.serverPosZ);
+		int blockX = MathHelper.floor(e.getPosX());
+		int blockY = MathHelper.floor(e.getPosY()-0.2D - e.getYOffset());
+		int blockZ = MathHelper.floor(e.getPosZ());
 		BlockPos blockpos = new BlockPos(blockX, blockY, blockZ);
 		return e.world.getBlockState(blockpos).getBlock();
 	}
