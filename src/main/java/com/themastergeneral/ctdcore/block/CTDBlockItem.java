19659111f2509cc388067c14ca134d92722b2057
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-07-05 01:52:59 -0400

    Add Basic Block Item class for other mods to dep on

---
+++ src/main/java/com/themastergeneral/ctdcore/block/CTDBlockItem.java
@@ -0,0 +1,13 @@
+package com.themastergeneral.ctdcore.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+
+public class CTDBlockItem extends BlockItem
+{
+    public CTDBlockItem(Block blockIn, Properties properties)
+    {
+        super(blockIn, properties);
+    }
+
+}
