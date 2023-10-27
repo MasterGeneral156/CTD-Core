2181a9538f5ae8ea18a1847ae3a9736fc469a815
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-10-26 21:36:45 -0400

    Update ModUtils.java

--- src/main/java/com/themastergeneral/ctdcore/helpers/ModUtils.java
+++ src/main/java/com/themastergeneral/ctdcore/helpers/ModUtils.java
@@ -27,18 +27,63 @@
 */
 package com.themastergeneral.ctdcore.helpers;
 
+import java.text.NumberFormat;
+
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 
 public class ModUtils 
 {
+	/**
+	 * Helper function to display a string as a component.
+	 * @param string
+	 * @return Literal Component
+	 * @since 1.19.4-2.3.0
+	 */
 	public static MutableComponent displayString(String string)
 	{
 		return Component.literal(string);
 	}
 	
+	/**
+	 * Helper function to display a string translation key as a component.
+	 * @param string
+	 * @return Translatable Component
+	 * @since 1.19.4-2.3.0
+	 */
 	public static MutableComponent displayTranslation(String string)
 	{
 		return Component.translatable(string);
+	}
+	
+	/**
+	 * Makes the input number into a friendly readable number (IE: 1K, 200K, 3M, etc.)
+	 * @param number
+	 * @return Formatted Number (IE: 1K, 200K, 3M)
+	 * @since 1.20.2-2.4.5
+	 */
+	public static String returnShortenedNumber(Number number)
+	{
+		Long numberLong = (Long) number.longValue();
+		NumberFormat nf = NumberFormat.getInstance();
+		String returned;
+		
+		if (numberLong < 1000)
+	        returned = nf.format(number);
+	    else if (number.intValue() < 10000L)
+	        returned = nf.format(numberLong / 1000L) + "K";
+	    else if (numberLong < 1000000L)
+	        returned = nf.format(numberLong / 1000L) + "K";
+	    else if (numberLong < 1000000000L)
+	        returned = nf.format(numberLong / 1000000L) + "M";
+	    else if (numberLong < 1000000000000L)
+	        returned = nf.format(numberLong / 1000000000L) + "B";
+	    else if (numberLong < 1000000000000000L)
+	        returned = nf.format(numberLong / 1000000000000L) + "T";
+	    else if (numberLong < 1000000000000000000L)
+	        returned = nf.format(numberLong / 1000000000000000L) + " Quadrillion";
+	    else
+	        returned = nf.format(number);
+	    return returned;
 	}
 }
