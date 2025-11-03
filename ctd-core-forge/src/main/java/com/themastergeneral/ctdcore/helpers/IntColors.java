82990d908fc6602b1d736391e7555a496dbae977
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-11-03 18:24:30 -0500

    Add color reference class

---
+++ ctd-core-forge/src/main/java/com/themastergeneral/ctdcore/helpers/IntColors.java
@@ -0,0 +1,53 @@
+/**
+ *  Project:	CTD Core 1.21
+ * 	File:		com.themastergeneral.ctdcore.helpers.IntColors
+ * 	Author:		TheMasterGeneral
+ * 	Website: 	https://github.com/MasterGeneral156/CTD-Core
+ * 	License:	MIT License
+ *
+ * 				Copyright (c) 2025 TheMasterGeneral
+ *
+ * 				Permission is hereby granted, free of charge, to any person obtaining a copy
+ * 				of this software and associated documentation files (the "Software"), to deal
+ * 				in the Software without restriction, including without limitation the rights
+ * 				to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * 				copies of the Software, and to permit persons to whom the Software is
+ * 				furnished to do so, subject to the following conditions:
+ *
+ * 				The above copyright notice and this permission notice shall be included in all
+ * 				copies or substantial portions of the Software.
+ *
+ * 				THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * 				IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * 				FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * 				AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * 				LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * 				OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * 				SOFTWARE.
+ */
+package com.themastergeneral.ctdcore.helpers;
+
+/**
+ * Constants for colors that are visually similar to Vanilla's color palette.
+ * @return int RGB Color Code
+ * @since 1.21.10-2.9.3
+ */
+public class IntColors {
+
+    public static final int WHITE = 0xF9FFFE;
+    public static final int LIGHT_GRAY = 0x9D9D97;
+    public static final int GRAY = 0x474F52;
+    public static final int BLACK = 0x1D1D21;
+    public static final int BROWN = 0x835432;
+    public static final int RED = 0xB02E26;
+    public static final int ORANGE = 0xF9801D;
+    public static final int YELLOW = 0xFED83D;
+    public static final int LIME = 0x80C71F;
+    public static final int GREEN = 0x5E7C16;
+    public static final int CYAN = 0x169C9C;
+    public static final int LIGHT_BLUE = 0x3AB3DA;
+    public static final int BLUE = 0x3C44AA;
+    public static final int PURPLE = 0x8932B8;
+    public static final int MAGENTA = 0xC74EBD;
+    public static final int PINK = 0xF38BAA;
+}
