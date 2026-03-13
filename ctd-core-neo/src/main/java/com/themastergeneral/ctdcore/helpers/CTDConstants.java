9791a6f99b438ff4bbcb54ea2846c7b45639c047
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2026-03-13 00:14:51 -0400

    Sunny boi helpers

--- ctd-core-neo/src/main/java/com/themastergeneral/ctdcore/helpers/CTDConstants.java
+++ ctd-core-neo/src/main/java/com/themastergeneral/ctdcore/helpers/CTDConstants.java
@@ -42,8 +42,9 @@ public class CTDConstants {
     public static int saddleSlot    	= EquipmentSlot.SADDLE.getIndex();
 
     //Important Player UUIDs
-    public static String tmgUUID	= "ee1b5154-53c7-43df-99d3-4e8a7bac6d03";
-    public static String tmgAltUUID	= "2d2c2552-01bf-42fb-86cb-d788cb66ffb2";
+    public static String tmgUUID	    = "ee1b5154-53c7-43df-99d3-4e8a7bac6d03";   //TheMasterGeneral
+    public static String tmgAltUUID	    = "2d2c2552-01bf-42fb-86cb-d788cb66ffb2";   //ThePurpleGeneral
+    public static String sunnyUUID      = "656fbf9e-ebe1-4340-b219-e06470dc5dd0";   //SunnConure
 
     //Creative Durability
     public static int creativeDurability = Short.MAX_VALUE;
--- ctd-core-neo/src/main/java/com/themastergeneral/ctdcore/helpers/EntityHelper.java
+++ ctd-core-neo/src/main/java/com/themastergeneral/ctdcore/helpers/EntityHelper.java
@@ -97,4 +97,26 @@ public class EntityHelper {
     {
         return isMasterGeneral(player.getStringUUID());
     }
+
+    /**
+     * Tests the UUID to see if its REALLY the Sunny boi...
+     * @param String playerUUID
+     * @return bool
+     * @since 26.1-snapshot-2.9.4
+     */
+    public static boolean isSunnyBoi(String playerUUID)
+    {
+        return (playerUUID == CTDConstants.sunnyUUID);
+    }
+
+    /**
+     * Tests the Player Entity to see if its REALLY Sunny boi...
+     * @param Player player
+     * @return bool
+     * @since 26.1-snapshot-2.9.4
+     */
+    public static boolean isSunnyBoi(Player player)
+    {
+        return isSunnyBoi(player.getStringUUID());
+    }
 }
